047ec3378054987f7d45bc45aa44df734eb8ffbf
Chandan <90612000+Pogopeck@users.noreply.github.com>
2025-08-12 18:41:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 COHERE_API_KEY = os.getenv("COHERE_API_KEY")
-# Or use Hugging Face for open models
-HUGGINGFACE_TOKEN = os.getenv("HUGGINGFACE_TOKEN")
-DATA_PATH = "data/sample_docs"
-CHROMA_DB = "chroma_db"
+DATA_PATH = os.getenv("DATA_PATH", "data/sample_docs")
+CHROMA_DB = os.getenv("CHROMA_DB", "chroma_db")
